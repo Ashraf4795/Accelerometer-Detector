9e1665a3a16799c8dfe92cf87fa359b22eaba39c
ashraf4747 <ashraf.mohamediti4728@gmail.com>
2022-08-04 13:48:48 +0200

    resolve conflicts

--- src/main/java/com/ashrafmohamed/accelerometer_detector/detectors/shake_detector/ShakeDetector.kt
+++ src/main/java/com/ashrafmohamed/accelerometer_detector/detectors/shake_detector/ShakeDetector.kt
@@ -1,11 +1,8 @@
 package com.ashrafmohamed.accelerometer_detector.detectors.shake_detector
 
-import android.hardware.Sensor
 import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import com.ashrafmohamed.accelerometer_detector.detectors.Rule
 import com.ashrafmohamed.accelerometer_detector.detectors.Detector
-import com.ashrafmohamed.accelerometer_detector.detectors.rules.Rule
+import com.ashrafmohamed.accelerometer_detector.detectors.Rule
 
 class ShakeDetector(private val rule: Rule, private val callback: (event: SensorEvent) -> Unit) : Detector() {
 
