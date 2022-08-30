6b36758fd55d422e80a21b7c409136a22f07cc4a
ashraf.hassan@vodafone.com <ashraf.hassan@vodafone.com>
2022-08-30 15:37:33 +0200

    remove dummy comments

--- src/main/java/com/ashrafmohamed/accelerometer_detector/detectors/detector/ShakeDetector.kt
+++ src/main/java/com/ashrafmohamed/accelerometer_detector/detectors/detector/ShakeDetector.kt
@@ -5,7 +5,7 @@ import com.ashrafmohamed.accelerometer_detector.detectors.Detector
 import com.ashrafmohamed.accelerometer_detector.detectors.rules.Rule
 
 class ShakeDetector(private val rule: Rule, private val callback: (event: SensorEvent) -> Unit) : Detector() {
-
+    // add tag
     override fun onSensorChanged(event: SensorEvent) {
         if (rule.evaluate(event))
             callback.invoke(event)
